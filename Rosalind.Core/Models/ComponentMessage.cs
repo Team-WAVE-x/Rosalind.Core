1aa282fa2aa39d5d19971ff8fb882124a2b3232a
Kate Lin <63230494+KateLin-BASIC@users.noreply.github.com>
2021-09-16 19:32:26 +0900

    루루라라

---
+++ Rosalind.Core/Models/ComponentMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rosalind.Core.Models
+{
+    public class ComponentMessage
+    {
+    }
+}
---
+++ Rosalind.Core/Services/ComponentService.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace Rosalind.Core.Services
+{
+    public class ComponentService
+    {
+        private IServiceProvider _service;
+        private DiscordSocketClient _client;
+
+        public ComponentService(IServiceProvider service)
+        {
+            _service = service;
+            _client = service.GetRequiredService<DiscordSocketClient>();
+            _client.InteractionCreated += OnInteractionCreated;
+        }
+
+        public void SendComponentMessage(SocketCommandContext context, Dictionary<ComponentBuilder, Action> dictionaries, TimeSpan timeout, bool removeMessageAfterTimeOut = false)
+        {
+            var cache = MemoryCache.Default;
+            var policy = new CacheItemPolicy { SlidingExpiration = timeout, RemovedCallback = CacheRemovedCallback };
+        }
+
+        private Task OnInteractionCreated(SocketInteraction arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
