c5236d6cb913cf2b5f67e2835255a70cad98775d
Kate Lin <63230494+KateLin-BASIC@users.noreply.github.com>
2021-09-17 21:48:56 +0900

    모르겟음.. 몬가.. 일어나고 잇음

--- Rosalind.Core/Services/ComponentService.cs
+++ Rosalind.Core/Services/ComponentService.cs
@@ -21,10 +21,25 @@ namespace Rosalind.Core.Services
             _client.InteractionCreated += OnInteractionCreated;
         }
 
-        public void SendComponentMessage(SocketCommandContext context, Dictionary<ComponentBuilder, Action> dictionaries, TimeSpan timeout, bool removeMessageAfterTimeOut = false)
+        /// <summary>
+        /// 컴포넌트 메시지를 전송하고 캐싱합니다.
+        /// </summary>
+        /// <param name="content">메시지의 내용</param>
+        /// <param name="context">커맨드의 컨텍스트</param>
+        /// <param name="dictionaries">버튼의 토큰이 키고 대리자가 값인 딕셔너리</param>
+        /// <param name="embed">메시지에 부착할 임베드</param>
+        /// <param name="seconds">캐시 시간</param>
+        /// <param name="removeMessageAfterTimeOut">캐시 시간 초과시 메시지 삭제 여부</param>
+        public void SendComponentMessage(
+            string content,
+            SocketCommandContext context,
+            Dictionary<string, Action> dictionaries,
+            Embed embed = null,
+            ulong seconds = 500,
+            bool removeMessageAfterTimeOut = false)
         {
             var cache = MemoryCache.Default;
-            var policy = new CacheItemPolicy { SlidingExpiration = timeout, RemovedCallback = CacheRemovedCallback };
+            var policy = new CacheItemPolicy { SlidingExpiration = TimeSpan.FromSeconds(seconds), RemovedCallback = CacheRemovedCallback };
         }
 
         private Task OnInteractionCreated(SocketInteraction arg)
